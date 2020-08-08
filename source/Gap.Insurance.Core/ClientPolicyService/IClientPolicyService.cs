9304c68d06324f2ab450063e66216aa436cc25c1
Juan Cuartas <equisoide@gmail.com>
2020-08-07 22:27:50 -0500

    Created interface definition: IClientPolicyService

---
+++ source/Gap.Insurance.Core/ClientPolicyService/IClientPolicyService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Celerik.NetCore.Services;
+using Gap.Insurance.Model;
+
+namespace Gap.Insurance.Services
+{
+    public interface IClientPolicyService
+    {
+        Task<ApiResponse<IEnumerable<ClientPolicyDto>, GetClientPoliciesStatus>> GetClientPoliciesAsync(GetClientPoliciesPayload payload);
+        Task<ApiResponse<ClientPolicyDto, CreateClientPolicyStatus>> CreateClientPolicyAsync(CreatePolicyPayload payload);
+        Task<ApiResponse<ClientPolicyDto, CancelClientPolicyStatus>> CancelClientPolicyAsync(UpdatePolicyPayload payload);
+    }
+}
