052fadc5d4d39d640cb648571551dca6d92b4ffa
Juan Cuartas <equisoide@gmail.com>
2020-08-07 22:13:41 -0500

    Created interfac definition: IPolicyService

---
+++ source/Gap.Insurance.Core/PolicyService/IPolicyService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Celerik.NetCore.Services;
+using Gap.Insurance.Model;
+
+namespace Gap.Insurance.Services
+{
+    public interface IPolicyService
+    {
+        Task<ApiResponse<IEnumerable<PolicyDto>, GetPoliciesStatus>> GetPoliciesAsync();
+        Task<ApiResponse<PolicyDto, CreatePolicyStatus>> CreatePolicyAsync(CreatePolicyPayload payload);
+        Task<ApiResponse<PolicyDto, UpdatePolicyStatus>> UpdatePolicyAsync(UpdatePolicyPayload payload);
+        Task<ApiResponse<PolicyDto, DeletePolicyStatus>> DeletePolicyAsync(DeletePolicyPayload payload);
+    }
+}
