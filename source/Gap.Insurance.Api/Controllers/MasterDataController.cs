4d1abc72683492d4c75c7d3582a66ae7785e0323
Juan Cuartas <equisoide@gmail.com>
2020-08-08 11:31:24 -0500

    Created Api project with MasterData endpoints

---
+++ source/Gap.Insurance.Api/Controllers/MasterDataController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Celerik.NetCore.Services;
+using Gap.Insurance.Core;
+using Gap.Insurance.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gap.Insurance.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MasterDataController : ControllerBase
+    {
+        private readonly IMasterDataService _masterDataSvc;
+
+        public MasterDataController(IMasterDataService masterDataSvc)
+            => _masterDataSvc = masterDataSvc;
+
+        [HttpGet]
+        [Route("GetRisks")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ApiResponse<IEnumerable<RiskDto>, GetRisksStatus>>> GetRisks()
+            => Ok(await _masterDataSvc.GetRisksAsync());
+
+        [HttpGet]
+        [Route("GetCoverages")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ApiResponse<IEnumerable<CoverageDto>, GetCoveragesStatus>>> GetCoverages()
+            => Ok(await _masterDataSvc.GetCoveragesAsync());
+    }
+}
---
+++ source/Gap.Insurance.Api/Startup/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Celerik.NetCore.Web;
+using Gap.Insurance.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gap.Insurance.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+            => _config = config;
+
+        private string ApiName => _config["ApiName"];
+
+        public void ConfigureServices(IServiceCollection services) =>
+            MicroserviceStartup.ConfigureServices(services, _config, ApiName, () =>
+            {
+                services.AddCoreServices(_config);
+            });
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            => MicroserviceStartup.Configure(app, env, _config, ApiName);
+    }
+}
