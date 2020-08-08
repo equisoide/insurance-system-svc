51355bf02da6afb71a010d4257c8d7b22fb897c0
Juan Cuartas <equisoide@gmail.com>
2020-08-07 22:56:04 -0500

    Added resource files

---
+++ source/Gap.Insurance.Core/Resources/CoreResources.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using Celerik.NetCore.Util;
+using Microsoft.Extensions.Localization;
+
+namespace Gap.Insurance.Core
+{
+    [SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "This class is instantiable by the String Localizer Factory")]
+    [ExcludeFromCodeCoverage]
+    public class CoreResources
+    {
+        private static IStringLocalizer _localizer;
+
+        private static IStringLocalizer Localizer
+        {
+            get
+            {
+                if (_localizer == null && UtilResources.Factory != null)
+                    _localizer = UtilResources.Factory.Create(typeof(CoreResources));
+
+                return _localizer;
+            }
+        }
+
+        public static string Get(string name)
+            => Localizer?[name].Value ?? name;
+
+        public static string Get(string name, params object[] arguments)
+            => Localizer?[name, arguments].Value ??
+                string.Format(CultureInfo.InvariantCulture, name, arguments);
+    }
+}
