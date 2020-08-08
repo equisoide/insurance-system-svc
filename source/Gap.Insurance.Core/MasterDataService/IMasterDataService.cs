cba5357ffafcdccfe30fe21743489dd1864035c7
Juan Cuartas <equisoide@gmail.com>
2020-08-07 22:07:06 -0500

    Created interface definition: IMasterDataService

---
+++ source/Gap.Insurance.Core/MasterDataService/IMasterDataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Celerik.NetCore.Services;
+using Gap.Insurance.Model;
+
+namespace Gap.Insurance.Services
+{
+    public interface IMasterDataService
+    {
+        Task<ApiResponse<IEnumerable<RiskDto>, GetRisksStatus>> GetRisksAsync();
+        Task<ApiResponse<IEnumerable<CoverageDto>, GetCoveragesStatus>> GetCoveragesAsync();
+    }
+}
