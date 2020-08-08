61ead99db4e7133e50aeffbf308700b3c7956648
Juan Cuartas <equisoide@gmail.com>
2020-08-07 22:22:25 -0500

    Created interface definition: ICoverageService

---
+++ source/Gap.Insurance.Core/CoverageService/ICoverageService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Celerik.NetCore.Services;
+using Gap.Insurance.Model;
+
+namespace Gap.Insurance.Services
+{
+    public interface ICoverageService
+    {
+        Task<ApiResponse<PolicyCoverageDto, CreatePolicyCoverageStatus>> CreatePolicyCoverageAsync(CreatePolicyCoveragePayload payload);
+        Task<ApiResponse<PolicyCoverageDto, UpdatePolicyCoverageStatus>> UpdatePolicyCoverageAsync(UpdatePolicyCoveragePayload payload);
+        Task<ApiResponse<PolicyCoverageDto, DeletePolicyCoverageStatus>> DeletePolicyCoverageAsync(DeletePolicyCoveragePayload payload);
+    }
+}
